f367c7e2b0fa917aceb68ed1c997a0502e553543
caiyalynn <112013328+caiyalynn@users.noreply.github.com>
2024-02-07 11:33:47 -0600

    added new files

--- src/GitLab/src/unlraikes/src/GitLab.java
+++ src/GitLab/src/unlraikes/src/GitLab.java
@@ -1,5 +1,8 @@
-public class App {
+public class GitLab {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
 }
+
+GitLabHelper helper = new GitLabHelper("Name1", "Name2");
+helper.PrintWelcomeMessage();
---
+++ src/GitLab/src/unlraikes/src/main/java/edu/unl/raikes/git/Main.java
@@ -0,0 +1,7 @@
+package edu.unl.raikes.git;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
